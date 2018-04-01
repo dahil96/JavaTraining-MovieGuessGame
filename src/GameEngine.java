ae8af2164d5046b083dedcdf1838fca921244ac2
Daniel-Laptop <dhilmersson@outlook.com>
2018-04-01 22:26:51 +0200

    first version of full game

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,21 +1,26 @@
 import java.io.File;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class GameEngine {
 
-    private int guessesLeft;
+    private int nrOfGuessesLeft;
     private String selectedTitle;
     private String[] movieNames;
     private String[] currentRightAnswers;
     private String currentWrongGuesses;
+    private boolean isGameLive;
+    private boolean playerWon;
 
     public GameEngine() {
-        guessesLeft = 0;
+        nrOfGuessesLeft = 0;
         movieNames = loadAvailableTitles();
         selectedTitle = this.getNewTitle();
         currentRightAnswers = null;
         currentWrongGuesses = "";
+        isGameLive = true;
+        playerWon = false;
     }
     private String[] loadAvailableTitles(){
         File fileHandle = new File("movies.txt");
@@ -49,7 +54,7 @@ public class GameEngine {
         return loadedMovieNames;
     }
     private String[] buildNewAnswerString() {
-        String[] answerString = new String[selectedTitle.length()-1];
+        String[] answerString = new String[selectedTitle.length()];
         for (int i = 0; i < answerString.length; i++) {
             if( Character.isWhitespace(selectedTitle.charAt((i)))){
                 answerString[i] = "_";
@@ -59,6 +64,13 @@ public class GameEngine {
         }
         return answerString;
     }
+    private void updateCurrentRightAnswers(String guess) {
+        for (int i = 0; i < selectedTitle.length();i++) {
+            if (selectedTitle.charAt(i) == guess.charAt(0)){
+                currentRightAnswers[i] = guess;
+            }
+        }
+    }
     private String getNewTitle() {
         //Select a random movie title
         Random randomGenerator = new Random();
@@ -66,28 +78,65 @@ public class GameEngine {
         return movieNames[randomIndex];
     }
 
+    private void assesGameStatus() {
+        System.out.println("Assessing game state");
+        if(nrOfGuessesLeft == 0) {
+            isGameLive = false;
+            playerWon = false;
+        } else if (!Arrays.asList(currentRightAnswers).contains("-")) {
+            isGameLive = false;
+            playerWon = true;
+        }
+    }
+
     public void startNewGame(int nrOfGuesses) {
         //Setup new game
-        guessesLeft = nrOfGuesses;
+        nrOfGuessesLeft = nrOfGuesses;
         currentWrongGuesses = "";
         currentRightAnswers = buildNewAnswerString();
         System.out.println("Selected movie title is: " + selectedTitle);
     }
 
-    public int makeGuess(String guess) {
-        if (guessesLeft > 0) {
+    public boolean makeGuess(String guess) {
+        boolean wasGuessCorrect = false;
+        if (nrOfGuessesLeft > 0) {
             if(selectedTitle.contains(guess)) {
-                System.out.println("contains");
+                updateCurrentRightAnswers(guess);
+                wasGuessCorrect = true;
+            } else {
+                nrOfGuessesLeft--;
+                currentWrongGuesses = currentWrongGuesses + guess + ",";
+                wasGuessCorrect = false;
             }
-            guessesLeft--;
         }
-        return guessesLeft;
+        assesGameStatus();
+        return wasGuessCorrect;
     }
-    public int noOfGuessesLeft() {
-        return guessesLeft;
+    public int getNoOfGuessesLeft() {
+        return nrOfGuessesLeft;
+    }
+
+    public boolean isGameStillLive() {
+        return isGameLive;
     }
 
     public String getCurrentRightAnswers () {
         return String.join(" ",currentRightAnswers);
+    }
+
+    public String getCurrentWrongGuesses(){
+        return currentWrongGuesses;
+    }
+
+    public boolean didPlayerWin() {
+        return playerWon;
+    }
+
+    public String getMovieTitle() {
+        if (isGameLive) {
+            return "Error, game is still running";
+        } else {
+            return selectedTitle;
+        }
     }
 }
--- src/MovieGuessGame.java
+++ src/MovieGuessGame.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.util.Arrays;
-import java.util.Random;
+
 import java.util.Scanner;
 
 
@@ -18,6 +16,29 @@ public class MovieGuessGame {
         System.out.println("The title to guess has the following letters");
         System.out.println(movieGameEngine.getCurrentRightAnswers());
 
+        boolean lastGuess;
+        Scanner input = new Scanner(System.in);
+        String guess = "";
+        do {
+            System.out.println("--------------------------------");
+            System.out.println("Make your guess! Choose a letter");
+            guess = input.nextLine();
+            lastGuess = movieGameEngine.makeGuess(guess);
+            System.out.printf("The guess was %s%n", lastGuess);
+            System.out.println("Current score is: ");
+            System.out.println("Number of guesses left " + movieGameEngine.getNoOfGuessesLeft());
+            System.out.println("The current guesses are " + movieGameEngine.getCurrentRightAnswers());
+            System.out.println("Current wrong letters guessed " + movieGameEngine.getCurrentWrongGuesses());
+        }
+        while (movieGameEngine.isGameStillLive());
+        input.close();
+        System.out.println("The game ended:");
+        if (movieGameEngine.didPlayerWin()) {
+            System.out.println("Great Job the title was " + movieGameEngine.getMovieTitle());
+            System.out.println("You guessed it with " + movieGameEngine.getNoOfGuessesLeft() + " guesses left");
+        } else {
+            System.out.println("Sorry you did not guess the title, the title was " + movieGameEngine.getMovieTitle());
+        }
         // Game logic, if correct letter is guessed, its shown
         // If wrong letter, letter is shown and one point is removed
         // Game ends if you have guessed wrong 10 times or you have guessed all letters right
