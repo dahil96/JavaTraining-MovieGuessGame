f808b7a6909b456f9ab9f80574f7a9ef39bc9b84
Daniel-Laptop <dhilmersson@outlook.com>
2018-04-01 14:20:02 +0200

    Update

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -4,62 +4,67 @@ import java.util.Scanner;
 
 public class GameEngine {
 
-    private int guessesLeft = 0;
-    private String selectedTitle = "";
+    private int guessesLeft;
+    private String selectedTitle;
+    private String[] movieNames;
 
-    public GameEngine(int nrOfGuesses) {
-        guessesLeft = nrOfGuesses;
+    public GameEngine() {
+        guessesLeft = 0;
+        movieNames = loadAvailableTitles();
         selectedTitle = this.getNewTitle();
     }
+    private String[] loadAvailableTitles(){
+        File fileHandle = new File("movies.txt");
+        int rowsInDataset = 0;
+        String[] loadedMovieNames = null;
+        try {
+            //If file exist open it
+            if (fileHandle.exists()) {
+                Scanner fileReaderCountRows = new Scanner(fileHandle);
+                //Find out number of rows in dataset do define correct size of array
 
-    private String getNewTitle() {
-        return "test";
+                while (fileReaderCountRows.hasNextLine()) {
+                    fileReaderCountRows.nextLine();
+                    rowsInDataset++;
+                }
+                //Create the dataset
+                loadedMovieNames = new String[rowsInDataset];
+                //Run the file scanner to read content
+                Scanner fileReaderReadContent = new Scanner(fileHandle);
+                int index = 0;
+                while (fileReaderReadContent.hasNextLine()) {
+                    loadedMovieNames[index] = fileReaderReadContent.nextLine();
+                    index++;
+                }
+            } else {
+                System.out.println("File does not exist");
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong during file operations the file" + e.toString());
+        }
+        return loadedMovieNames;
     }
+    private String getNewTitle() {
+        //Select a random movie title
+        Random randomGenerator = new Random();
+        int randomIndex = randomGenerator.nextInt(movieNames.length - 1);
+        return movieNames[randomIndex];
+    }
+
+    public boolean startNewGame() {
+        return true;
+    }
+    /*
+        //hold current letters from wrong guesses
+        String wrongGuesses = "";
+
+        //Hold representation of current right guesses
+        char[] currentGuess = new char[selectedTitle.length()];
+        for (int i = 0; i < selectedTitle.length(); i++) {
+            currentGuess[i] = '_';
+        }*/
 
     public String getMoveTile() {
         return selectedTitle;
     }
-/*
-
-    File fileHandle = new File("movies.txt");
-    String[] movieNames = null;
-    int rowsInDataset = 0;
-        try {
-        //If file exist open it
-        if (fileHandle.exists()) {
-            Scanner fileReaderCountRows = new Scanner(fileHandle);
-            //Find out number of rows in dataset do define correct size of array
-
-            while (fileReaderCountRows.hasNextLine()) {
-                fileReaderCountRows.nextLine();
-                rowsInDataset++;
-            }
-            //Create the dataset
-            movieNames = new String[rowsInDataset];
-            //Run the file scanner to read content
-            Scanner fileReaderReadContent = new Scanner(fileHandle);
-            int index = 0;
-            while (fileReaderReadContent.hasNextLine()) {
-                movieNames[index] = fileReaderReadContent.nextLine();
-                index++;
-            }
-        } else {
-            System.out.println("File does not exist");
-        }
-    } catch (Exception e) {
-        System.out.println("Something went wrong during file operations the file" + e.toString());
-    }
-
-    //Select a random movie title
-    Random randomGenerator = new Random();
-    int randomIndex = randomGenerator.nextInt(rowsInDataset-1);
-    String selectedTitle = movieNames[randomIndex];
-    //hold current letters from wrong guesses
-    String wrongGuesses = "";
-    //Hold representation of current right guesses
-    char[] currentGuess = new char[selectedTitle.length()];
-        for (int i = 0; i < selectedTitle.length(); i++) {
-        currentGuess[i] = '_';
-    }
-    */
 }
--- src/MovieGuessGame.java
+++ src/MovieGuessGame.java
@@ -10,48 +10,8 @@ public class MovieGuessGame {
 
     public static void main (String args[]) {
 
-        GameEngine movieGameEngine = new GameEngine(10);
-        //Open and parse file into dataset
-        /* File fileHandle = new File("movies.txt");
-       String[] movieNames = null;
-        int rowsInDataset = 0;
-        try {
-            //If file exist open it
-            if (fileHandle.exists()) {
-                Scanner fileReaderCountRows = new Scanner(fileHandle);
-                //Find out number of rows in dataset do define correct size of array
+        GameEngine movieGameEngine = new GameEngine();
 
-                while (fileReaderCountRows.hasNextLine()) {
-                    fileReaderCountRows.nextLine();
-                    rowsInDataset++;
-                }
-                //Create the dataset
-                movieNames = new String[rowsInDataset];
-                //Run the file scanner to read content
-                Scanner fileReaderReadContent = new Scanner(fileHandle);
-                int index = 0;
-                while (fileReaderReadContent.hasNextLine()) {
-                    movieNames[index] = fileReaderReadContent.nextLine();
-                    index++;
-                }
-            } else {
-                System.out.println("File does not exist");
-            }
-        } catch (Exception e) {
-            System.out.println("Something went wrong during file operations the file" + e.toString());
-        }
-
-        //Select a random movie title
-        Random randomGenerator = new Random();
-        int randomIndex = randomGenerator.nextInt(rowsInDataset-1);
-        String selectedTitle = movieNames[randomIndex];
-        //hold current letters from wrong guesses
-        String wrongGuesses = "";
-        //Hold representation of current right guesses
-        char[] currentGuess = new char[selectedTitle.length()];
-        for (int i = 0; i < selectedTitle.length(); i++) {
-            currentGuess[i] = '_';
-        }*/
 
         System.out.println("Selected title is " + movieGameEngine.getMoveTile() );
         //UI on the game
